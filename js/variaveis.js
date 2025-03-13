6342ef378a3c4f135120dc9ccbe876cbdee05d08
Nickddb <nicollerocha.dev2024@gmail.com>
2025-03-13 11:22:19 -0300

    variaveis

---
+++ js/variaveis.js
@@ -0,0 +1,11 @@
+function variavel(){
+    if (true){
+        //def variáveis
+        var x = 10;
+        let y = 20;
+        const z = 30;
+    }
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
