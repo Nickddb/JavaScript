31328cfd99e415645d63aff7af51bfe7fd8b14e3
Nickddb <nicollerocha.dev2024@gmail.com>
2025-03-18 09:18:44 -0300

    atualizacao

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,3 +1,13 @@
 function calcular(){
+    let num = 0;
+    let num1 = 1;
+    let num2 = 2;
+    let num3 = 3;
+    let num4 = 4;
+    let num5 = 5;
+    let num6 = 6;
+    let num7 = 7;
+    let num8 = 8;
+    let num9 = 9;
     
 }
