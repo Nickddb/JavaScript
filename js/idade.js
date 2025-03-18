5e0a926a8dae6608098116d12c54252278e16b6d
Nickddb <nicollerocha.dev2024@gmail.com>
2025-03-18 08:07:47 -0300

    atualizacao

--- js/idade.js
+++ js/idade.js
@@ -2,23 +2,23 @@ function verificar(){
     let idade = document.getElementById("idade").value;
     let nome = document.getElementById("nome").value;
     if (idade <= 2){
-        document.getElementById("bebe").textContent = nome + " é um bebê!";
-        console.log("If!");
+        document.getElementById("checar").textContent = nome + " é um bebê!";
+        console.log("bebe");
     }
-    else if (idade >= 2 || idade <= 12){
-        document.getElementById("crianza").textContent = nome + " é uma criança! ";
-        console.log("Else if!");
+    else if (idade >= 2 && idade <= 12){
+        document.getElementById("checar").textContent = nome + " é uma criança! ";
+        console.log("crianza");
     }
-    else if (idade >= 11 || idade <= 19){
-        document.getElementById("aborrescente").textContent = nome + " é um adolecente! ";
-        console.log("Else f!");
+    else if (idade >= 11 && idade <= 19){
+        document.getElementById("checar").textContent = nome + " é um adolecente! ";
+        console.log("aborrescente");
     }
-    else if (idade >= 19 || idade <= 60){
-        document.getElementById("adolto").textContent = nome + " é um adulto! ";
-        console.log("Else if!");
+    else if (idade >= 19 && idade <= 60){
+        document.getElementById("checar").textContent = nome + " é um adulto! ";
+        console.log("adolto");
     }
     else if (idade >= 60){
-        document.getElementById("melhor idade").textContent = nome + " está na melhor idade! ";
-        console.log("Else if!");
+        document.getElementById("checar").textContent = nome + " está na melhor idade! ";
+        console.log("melhor idade");
     }
 }
